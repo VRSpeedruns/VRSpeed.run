24d378abe277866bccae36479e2e4b089ad1c812
bigfoott <8753187+bigfoott@users.noreply.github.com>
2021-09-05 06:32:50 -0400

    added windlands_2, fixed indentation of getCookie function

--- vrsrassets/js/main.js
+++ vrsrassets/js/main.js
@@ -381,18 +381,22 @@ function setCookie(cname, cvalue, minutes) {
     let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-function getCookie(cname) {
+function getCookie(cname)
+{
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    for (let i = 0; i <ca.length; i++)
+    {
+        let c = ca[i];
+        while (c.charAt(0) == ' ')
+        {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0)
+        {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
+}
